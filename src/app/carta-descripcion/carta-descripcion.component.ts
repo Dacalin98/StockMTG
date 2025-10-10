6e6a4375d893ccec3d484e010e8aca89ed0931cb
Dacalin98 <davidcampos1998d@gmail.com>
2025-10-10 23:50:20 +0200

    Añadidas imagenes

--- src/app/carta-descripcion/carta-descripcion.component.ts
+++ src/app/carta-descripcion/carta-descripcion.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Carta } from '../models/carta.model';
 import { ComunicacionService } from '../services/comunicacion.service';
 import { NgClass } from "@angular/common";
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-carta-descripcion',
@@ -10,14 +11,23 @@ import { NgClass } from "@angular/common";
   styleUrl: './carta-descripcion.component.css',
 })
 export class CartaDescripcionComponent {
-  constructor(private comunicacionService: ComunicacionService) {}
+  constructor(
+    private comunicacionService: ComunicacionService,
+    private sanitizer: DomSanitizer
+  ) {}
   @Input()
-  carta: Carta | null= null;
+  carta: Carta | null = null;
   cantidad = 0;
+  texto: string = '';
+  textoHTML: SafeHtml | undefined;
 
   ngOnInit(): void {
     console.log(document.cookie);
-    this.carta = this.comunicacionService.cartaSeleccionada?? JSON.parse(localStorage.getItem('cartaSeleccionada') || 'null');
+    this.carta =
+      this.comunicacionService.cartaSeleccionada ??
+      JSON.parse(localStorage.getItem('cartaSeleccionada') || 'null');
+    this.procesarTexto();
+    this.textoHTML=this.sanitizer.bypassSecurityTrustHtml(this.texto);
   }
   aumentar() {
     this.cantidad++;
@@ -25,5 +35,16 @@ export class CartaDescripcionComponent {
 
   disminuir() {
     if (this.cantidad > 0) this.cantidad--;
+  }
+
+  procesarTexto() {
+    if (!this.carta?.oracle_text) return '';
+    var img = "<img src='/imgs/";
+    var expresion = /\{(.*?)\}/g;
+    this.texto = "<p>"+this.carta.oracle_text.replaceAll(expresion, (i) => {
+      return img  + i.substring(1, i.length - 1) + ".webp' style='height: 1em;'/>";
+    })+"</p>";
+    this.texto = this.texto.replaceAll("\n", "<br/>");
+    return
   }
 }
